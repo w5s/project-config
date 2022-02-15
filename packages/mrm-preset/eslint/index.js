a32e0fb739cbe349565b26ea412cdf37e3d56bdb
Julien Polo <julien.polo@koober.com>
2022-02-15 22:01:12 +0100

    fix: correct eslint vscode settings

--- packages/mrm-preset/eslint/index.js
+++ packages/mrm-preset/eslint/index.js
@@ -92,7 +92,7 @@ function createESLint({ eslintPreset: eslintPresetDefault = 'eslint:recommended'
         'editor.codeActionsOnSave': settings['editor.codeActionsOnSave'] || {
           'source.fixAll': true,
         },
-        'eslint.validate': extList.map(
+        'eslint.probe': extList.map(
           (ext) =>
             ({
               jsx: 'javascriptreact',
