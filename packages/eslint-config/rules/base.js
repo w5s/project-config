c79fd34f5d8fd2e91f5c6ad61436e41bb09378c4
Julien Polo <julien.polo@koober.com>
2022-02-20 22:55:02 +0100

    fix: ignore underscore case arguments

--- packages/eslint-config/rules/base.js
+++ packages/eslint-config/rules/base.js
@@ -36,6 +36,8 @@ module.exports = concatESConfig(
       ),
       // underscore is often used (mongodb, etc)
       'no-underscore-dangle': off,
+      // Ignore underscore case arguments
+      'no-unused-vars': [error, { argsIgnorePattern: '^_' }],
       // Allow in some cases https://github.com/airbnb/javascript/issues/1089#issuecomment-1024351821
       'no-use-before-define': [error, 'nofunc'],
     },
