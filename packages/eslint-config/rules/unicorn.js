39de3efeef075c42d255a63938b1a50d23bdcfe2
Julien Polo <julien.polo@koober.com>
2022-03-02 16:04:46 +0100

    fix: disable unicorn/consistent-function-scoping

--- packages/eslint-config/rules/unicorn.js
+++ packages/eslint-config/rules/unicorn.js
@@ -49,6 +49,7 @@ module.exports = concatESConfig(
   {
     rules: {
       'unicorn/consistent-destructuring': off,
+      'unicorn/consistent-function-scoping': off, // Too many false positive
       'unicorn/no-array-callback-reference': off, // Many false positive reported
       'unicorn/no-array-for-each': off, // This rule could change browser compatibility
       'unicorn/no-array-method-this-argument': off, // Many false positive reported
