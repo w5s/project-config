6d8ebcfb0315e31104f771481b3368a8fcaaccea
Julien Polo <julien.polo@gmail.com>
2022-08-16 22:02:41 +0200

    ✨ Add release github workflow

--- packages/mrm-preset/ci/github.js
+++ packages/mrm-preset/ci/github.js
@@ -6,6 +6,13 @@ const pkg = require('../core/pkg.js');
 function task() {
   const state = 'present';
   const packageManager = pkg.manager(packageJson());
+  const baseBranch = 'main';
+  const packageInstall =
+    packageManager === 'npm'
+      ? `${packageManager} ci`
+      : packageManager === 'yarn'
+      ? `${packageManager} install --frozen-lockfile`
+      : `${packageManager} install`;
   githubCI.workflow({
     name: 'node.js.yml',
     state,
@@ -14,10 +21,10 @@ function task() {
       ...config,
       on: {
         push: {
-          branches: ['main'],
+          branches: [baseBranch],
         },
         pull_request: {
-          branches: ['main'],
+          branches: [baseBranch],
         },
       },
       jobs: {
@@ -34,9 +41,58 @@ function task() {
                 cache: packageManager,
               },
             },
-            { run: `${packageManager} install` },
+            { run: packageInstall },
             { run: `${packageManager} run build` },
             { run: `${packageManager} validate` },
+          ],
+        },
+      },
+    }),
+  });
+
+  githubCI.workflow({
+    name: 'release.yml',
+    state,
+    update: (config) => ({
+      name: 'Release',
+      ...config,
+      on: {
+        push: {
+          branches: [baseBranch],
+        },
+      },
+      jobs: {
+        release: {
+          name: 'Release',
+          'runs-on': 'ubuntu-latest',
+          steps: [
+            { uses: 'actions/checkout@v3' },
+            {
+              name: 'Use Node.js',
+              uses: 'actions/setup-node@v3',
+              with: {
+                'node-version': 'lts/*',
+                cache: packageManager,
+              },
+            },
+            { run: packageInstall },
+            {
+              name: 'Create Release Pull Request or Publish to npm',
+              id: 'changesets',
+              uses: 'changesets/action@v1',
+              with: {
+                commit: '🔖 Publish package(s)',
+                title: '🔖 Publish package(s)',
+                publish: `${packageManager} release`,
+                version: `${packageManager} run version`,
+              },
+              env: {
+                // eslint-disable-next-line no-template-curly-in-string
+                GITHUB_TOKEN: '${{ secrets.GITHUB_TOKEN }}',
+                // eslint-disable-next-line no-template-curly-in-string
+                NPM_TOKEN: '${{ secrets.NPM_TOKEN }}',
+              },
+            },
           ],
         },
       },
