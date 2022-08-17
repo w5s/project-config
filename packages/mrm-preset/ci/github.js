4d618575bbe34c7a79f4caf2fc6a33c6e733c1ea
Julien Polo <julien.polo@gmail.com>
2022-08-17 21:59:13 +0200

    ⏪ Rollback to lerna

--- packages/mrm-preset/ci/github.js
+++ packages/mrm-preset/ci/github.js
@@ -49,55 +49,6 @@ function task() {
       },
     }),
   });
-
-  githubCI.workflow({
-    name: 'release.yml',
-    state,
-    update: (config) => ({
-      name: 'Release',
-      ...config,
-      on: {
-        push: {
-          branches: [baseBranch],
-        },
-      },
-      jobs: {
-        release: {
-          name: 'Release',
-          'runs-on': 'ubuntu-latest',
-          steps: [
-            { uses: 'actions/checkout@v3' },
-            {
-              name: 'Use Node.js',
-              uses: 'actions/setup-node@v3',
-              with: {
-                'node-version': 'lts/*',
-                cache: packageManager,
-              },
-            },
-            { run: packageInstall },
-            {
-              name: 'Create Release Pull Request or Publish to npm',
-              id: 'changesets',
-              uses: 'changesets/action@v1',
-              with: {
-                commit: '🔖 Publish package(s)',
-                title: '🔖 Publish package(s)',
-                publish: `${packageManager} release`,
-                version: `${packageManager} run version`,
-              },
-              env: {
-                // eslint-disable-next-line no-template-curly-in-string
-                GITHUB_TOKEN: '${{ secrets.GITHUB_TOKEN }}',
-                // eslint-disable-next-line no-template-curly-in-string
-                NPM_TOKEN: '${{ secrets.NPM_TOKEN }}',
-              },
-            },
-          ],
-        },
-      },
-    }),
-  });
 }
 
 module.exports = task;
