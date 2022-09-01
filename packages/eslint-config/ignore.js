8f65a55c378351ab3b4a8f154b9f9d5083aef344
Julien Polo <julien.polo@gmail.com>
2022-09-01 14:57:21 +0200

    ✨ Use gitignore as ignore pattern for eslint

--- packages/eslint-config/ignore.js
+++ packages/eslint-config/ignore.js
@@ -1,3 +1,39 @@
+// @ts-ignore
+const fs = require('node:fs');
+const findUp = require('find-up');
+const parseGitignore = require('parse-gitignore');
+
+const getGitignore = () => {
+  const found = findUp.sync('.gitignore');
+  if (found) {
+    return parseGitignore(fs.readFileSync(found));
+  }
+
+  return [];
+};
+
 module.exports = {
-  ignorePatterns: ['**/dist/**', '**/lib/**'],
+  ignorePatterns: [
+    '!.*',
+    '.yarn',
+    '.common/',
+    '.config/package-lock.json',
+    '.config/yarn.lock',
+    '.go/',
+    '.modules/',
+    '.pnpm-store/',
+    '.venv/',
+    'deprecated/',
+    'angular.json',
+    'esbuild.js',
+    'package-lock.json',
+    'pnpm-lock.yaml',
+    'slim.report.json',
+    'test-output/',
+    'venv/',
+    'yarn.lock',
+    '_generated_/',
+    '*.toml',
+    ...getGitignore(),
+  ],
 };
