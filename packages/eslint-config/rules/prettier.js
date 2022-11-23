01a353b84a1c58e7803cf7e39faeebc10f346151
Julien Polo <julien.polo@gmail.com>
2022-11-23 14:27:40 +0100

    ♻️ Facilitate organization name change

--- packages/eslint-config/rules/prettier.js
+++ packages/eslint-config/rules/prettier.js
@@ -1,25 +1,44 @@
 const { error } = require('./_rule.js');
 
-// Try require '@w5s/prettier-config'
-const prettierConfig = (() => {
+const getPackageScope = () => {
   try {
-    // eslint-disable-next-line global-require
-    return require('@w5s/prettier-config');
+    // eslint-disable-next-line global-require, import/no-dynamic-require
+    const { name } = require('../package.json');
+    const prefixMatch = (name || '').match(/(@\w+)\//);
+    const packageScope = prefixMatch ? prefixMatch[1] : undefined;
+    return packageScope;
   } catch (error_) {
     // eslint-disable-next-line no-console
     console.warn(error_);
-    /** @type {import('@w5s/prettier-config')} */
-    const defaultConfig = {
-      trailingComma: 'es5',
-    };
-    return defaultConfig;
+
+    return undefined;
   }
-})();
+};
+const getPrettierConfig = (/** @type {string} */ moduleName) => {
+  try {
+    /** @type {import('prettier').Config} */
+    // eslint-disable-next-line global-require, import/no-dynamic-require
+    const moduleConfig = require(moduleName);
+    return moduleConfig;
+  } catch {
+    return undefined;
+  }
+};
+
+// Try require '@my-organization/prettier-config'
+const getPrettierConfigDefault = () => {
+  /** @type {import('prettier').Config} */
+  const defaultConfig = {
+    trailingComma: 'es5',
+  };
+  const packageScope = getPackageScope();
+  return getPrettierConfig(`${packageScope}/prettier-config`) || defaultConfig;
+};
 
 module.exports = {
   extends: ['prettier'],
   plugins: ['prettier'],
   rules: {
-    'prettier/prettier': [error, prettierConfig],
+    'prettier/prettier': [error, getPrettierConfigDefault()],
   },
 };
