85a123628d82ee2567722795c4a11e4c50399d74
Julien Polo <julien.polo@gmail.com>
2022-11-23 12:34:16 +0100

    ♻️ Use shared prettier config

--- packages/eslint-config/rules/prettier.js
+++ packages/eslint-config/rules/prettier.js
@@ -1,15 +1,25 @@
 const { error } = require('./_rule.js');
 
+// Try require '@w5s/prettier-config'
+const prettierConfig = (() => {
+  try {
+    // eslint-disable-next-line global-require
+    return require('@w5s/prettier-config');
+  } catch (error_) {
+    // eslint-disable-next-line no-console
+    console.warn(error_);
+    /** @type {import('@w5s/prettier-config')} */
+    const defaultConfig = {
+      trailingComma: 'es5',
+    };
+    return defaultConfig;
+  }
+})();
+
 module.exports = {
   extends: ['prettier'],
   plugins: ['prettier'],
   rules: {
-    'prettier/prettier': [
-      error,
-      {
-        singleQuote: true,
-        trailingComma: 'es5',
-      },
-    ],
+    'prettier/prettier': [error, prettierConfig],
   },
 };
