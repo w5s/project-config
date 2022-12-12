c87e6d95501276d57a85f151e97c92b7db5905db
Julien Polo <julien.polo@gmail.com>
2022-12-12 15:56:43 +0100

    ✨ Disable @typescript-eslint/no-non-null-assertion for tests

--- packages/eslint-config/rules/jest.js
+++ packages/eslint-config/rules/jest.js
@@ -31,6 +31,7 @@ module.exports = concatESConfig(
    */
   {
     rules: {
+      '@typescript-eslint/no-non-null-assertion': off,
       '@typescript-eslint/no-unsafe-assignment': off,
       '@typescript-eslint/no-unsafe-call': off,
       '@typescript-eslint/no-unsafe-member-access': off,
