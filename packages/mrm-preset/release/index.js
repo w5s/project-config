cc3c1e3f20b3fd7c4412a3ac3ea199e005537468
Julien Polo <julien.polo@gmail.com>
2023-01-09 18:11:24 +0100

    Revert "💚 Correct clean build script"
    This reverts commit d09f705d0f3e92f292d5f9954528dbc0051565b3.

--- packages/mrm-preset/release/index.js
+++ packages/mrm-preset/release/index.js
@@ -11,9 +11,7 @@ function task() {
     pkg.script(packageFile, {
       name: project.release,
       // eslint-disable-next-line no-template-curly-in-string
-      update: useWorkspace
-        ? 'is-ci && npm run clean && lerna publish --yes || lerna publish'
-        : semanticRelease.command(),
+      update: useWorkspace ? 'is-ci && lerna publish --yes || lerna publish' : semanticRelease.command(),
       state: 'present',
     });
   });
