45afd493c118fda35e2ce701134ccc927317eca3
Julien Polo <julien.polo@gmail.com>
2023-01-13 22:07:03 +0100

    ♻️ Use constants

--- packages/eslint-config/jest.js
+++ packages/eslint-config/jest.js
@@ -1,12 +1,16 @@
-// http://eslint.org/docs/user-guide/configuring
+// eslint-disable-next-line import/no-unresolved
+const { EXTENSIONS_WITHOUT_DOT } = require('@w5s/dev');
+
+const extensions = EXTENSIONS_WITHOUT_DOT.join('|');
+
 module.exports = {
   overrides: [
     {
       extends: [require.resolve('./lib/jest.js')],
       files: [
-        '**/__mocks__/**/*.+(ts|tsx|js|jsx)',
-        '**/__tests__/**/*.+(ts|tsx|js|jsx)',
-        '**/?(*.)+(spec|test).+(ts|tsx|js|jsx)',
+        `**/__mocks__/**/*.+(${extensions})`,
+        `**/__tests__/**/*.+(${extensions})`,
+        `**/?(*.)+(spec|test).+(${extensions})`,
       ],
     },
   ],
--- packages/eslint-config/ts.js
+++ packages/eslint-config/ts.js
@@ -1,13 +1,15 @@
-// http://eslint.org/docs/user-guide/configuring
+// eslint-disable-next-line import/no-unresolved
+const { EXTENSIONS_WITHOUT_DOT } = require('@w5s/dev');
+
 module.exports = {
   overrides: [
     {
       extends: [
-        require.resolve('./lib/es.js'),
+        // require.resolve('./lib/es.js'),
         require.resolve('./lib/typescript.js'),
         require.resolve('./lib/prettier.js'),
       ],
-      files: ['*.+(ts|tsx)'],
+      files: [`*.+(${EXTENSIONS_WITHOUT_DOT.filter((_) => _.includes('ts')).join('|')})`],
     },
   ],
 };
