f764ccb672eeeb3b9e6a1821b2a1a546d9c28e7c
Julien Polo <julien.polo@gmail.com>
2023-04-19 17:03:27 +0200

    🐛 Add missing export

--- packages/conventional-changelog/lib/index.js
+++ packages/conventional-changelog/lib/index.js
@@ -1,5 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+/* eslint-disable import/no-import-module-exports */
 const git_raw_commit_opts_1 = require("./git-raw-commit-opts");
 const parser_opts_1 = require("./parser-opts");
 const recommended_bump_opts_1 = require("./recommended-bump-opts");
@@ -10,3 +11,5 @@ exports.default = {
     recommendedBumpOpts: recommended_bump_opts_1.recommendedBumpOpts,
     gitRawCommitOpts: git_raw_commit_opts_1.gitRawCommitOpts,
 };
+// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
+module.exports = exports.default;
--- packages/conventional-changelog/src/index.ts
+++ packages/conventional-changelog/src/index.ts
@@ -1,3 +1,4 @@
+/* eslint-disable import/no-import-module-exports */
 import { gitRawCommitOpts } from './git-raw-commit-opts';
 import { parserOpts } from './parser-opts';
 import { recommendedBumpOpts } from './recommended-bump-opts';
@@ -9,3 +10,6 @@ export default {
   recommendedBumpOpts,
   gitRawCommitOpts,
 };
+
+// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
+module.exports = exports.default;
