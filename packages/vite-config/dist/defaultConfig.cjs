6d9f49a6a08636fd5fb30440002d60900db86157
Julien Polo <julien.polo@gmail.com>
2023-06-23 09:28:58 +0200

    🧑‍💻 Add built files into git

---
+++ packages/vite-config/dist/defaultConfig.cjs
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.defaultConfig = void 0;
+const config_1 = require("vitest/config");
+exports.defaultConfig = Object.freeze({
+    test: config_1.configDefaults,
+});
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiZGVmYXVsdENvbmZpZy5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbIi4uLy4uLy4uL3NyYy9kZWZhdWx0Q29uZmlnLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7OztBQUFBLDBDQUErQztBQUVsQyxRQUFBLGFBQWEsR0FBRyxNQUFNLENBQUMsTUFBTSxDQUFDO0lBQ3pDLElBQUksRUFBRSx1QkFBYztDQUNyQixDQUFDLENBQUMifQ==
---
+++ packages/vite-config/dist/index.mjs
@@ -0,0 +1,2 @@
+export * from './defaultConfig.js';
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi8uLi8uLi9zcmMvaW5kZXgudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IkFBQUEsY0FBYyxvQkFBb0IsQ0FBQyJ9
