c5e9d0ef17f2facc869f258cf1185cec62ec5380
Julien Polo <julien.polo@gmail.com>
2023-07-22 22:44:09 +0200

    ♻️ Simplify prettier config resolving

--- packages/eslint-config/dist/prettier.js
+++ packages/eslint-config/dist/prettier.js
@@ -1,43 +1,13 @@
 "use strict";
-/* eslint-disable global-require */
-/* eslint-disable import/no-dynamic-require */
-/* eslint-disable @typescript-eslint/no-var-requires */
-/* eslint-disable @typescript-eslint/no-require-imports */
-const getPackageScope = () => {
-    try {
-        const { name } = require('../package.json');
-        const prefixMatch = (name ?? '').match(/(@\w+)\//);
-        const packageScope = prefixMatch == null ? undefined : prefixMatch[1];
-        return packageScope;
-    }
-    catch (error_) {
-        // eslint-disable-next-line no-console
-        console.warn(error_);
-        return undefined;
-    }
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
 };
-const getPrettierConfig = (moduleName) => {
-    try {
-        const moduleConfig = require(moduleName);
-        return moduleConfig;
-    }
-    catch {
-        return undefined;
-    }
-};
-// Try require '@my-organization/prettier-config'
-const getPrettierConfigDefault = () => {
-    const defaultConfig = {
-        trailingComma: 'es5',
-    };
-    const packageScope = getPackageScope();
-    return (packageScope == null ? undefined : getPrettierConfig(`${packageScope}/prettier-config`)) ?? defaultConfig;
-};
+const prettier_config_1 = __importDefault(require("@w5s/prettier-config"));
 const config = {
     extends: ['prettier'],
     plugins: ['prettier'],
     rules: {
-        'prettier/prettier': ['error', getPrettierConfigDefault()],
+        'prettier/prettier': ['error', prettier_config_1.default],
     },
 };
 module.exports = config;
