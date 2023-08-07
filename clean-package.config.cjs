16709d9c64b749d0512b660cec1f8aca0c8e5697
Julien Polo <julien.polo@gmail.com>
2023-08-07 23:11:05 +0200

    👷 Improve remove keys

--- clean-package.config.cjs
+++ clean-package.config.cjs
@@ -1,7 +1,19 @@
+const { readFileSync } = require('node:fs');
+
+const { scripts = {} } = JSON.parse(readFileSync(`${process.cwd()}/package.json`, 'utf8'));
+
 const cleanPackageConfig = {
   extends: ['clean-package/common'],
   indent: 2,
-  remove: ['commitlint', 'devDependencies', 'packageManager', 'stylelint'],
+  remove: [
+    'commitlint',
+    'devDependencies',
+    'packageManager',
+    'stylelint',
+    ...Object.keys(scripts)
+      .filter((_) => _ !== 'postpack')
+      .map((_) => `scripts.${_}`),
+  ],
 };
 
 module.exports = cleanPackageConfig;
