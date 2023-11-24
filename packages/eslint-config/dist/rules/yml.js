784db45ec83fe898f0de9f95572ad934b4d2764b
Julien Polo <julien.polo@gmail.com>
2023-11-24 10:24:21 +0100

    🐛 Disable yml/no-empty-mapping-value for docker-compose files

--- packages/eslint-config/dist/rules/yml.js
+++ packages/eslint-config/dist/rules/yml.js
@@ -1,6 +1,20 @@
 "use strict";
+const dev_1 = require("@w5s/dev");
+const yamlExtensions = dev_1.Project.queryExtensions(['yaml']);
 const config = {
     extends: ['plugin:yml/standard', 'plugin:yml/prettier'],
+    overrides: [
+        {
+            files: [
+                ...yamlExtensions.map((ext) => `docker-compose*${ext}`),
+                ...yamlExtensions.map((ext) => `compose-*${ext}`),
+                ...yamlExtensions.map((ext) => `compose${ext}`),
+            ],
+            rules: {
+                'yml/no-empty-mapping-value': 'off',
+            },
+        },
+    ],
     parser: 'yaml-eslint-parser',
     plugins: ['yml'],
 };
--- packages/eslint-config/src/rules/yml.ts
+++ packages/eslint-config/src/rules/yml.ts
@@ -1,7 +1,21 @@
+import { Project } from '@w5s/dev';
 import type eslint from 'eslint';
 
+const yamlExtensions = Project.queryExtensions(['yaml']);
 const config: eslint.Linter.Config = {
   extends: ['plugin:yml/standard', 'plugin:yml/prettier'],
+  overrides: [
+    {
+      files: [
+        ...yamlExtensions.map((ext) => `docker-compose*${ext}`),
+        ...yamlExtensions.map((ext) => `compose-*${ext}`),
+        ...yamlExtensions.map((ext) => `compose${ext}`),
+      ],
+      rules: {
+        'yml/no-empty-mapping-value': 'off',
+      },
+    },
+  ],
   parser: 'yaml-eslint-parser',
   plugins: ['yml'],
 };
