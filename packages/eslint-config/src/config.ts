14f8a64455fa56e7217805cf663bd5b96023e199
Julien Polo <julien.polo@gmail.com>
2025-01-09 23:13:03 +0100

    ♻️ Port import plugin

--- packages/eslint-config/src/config.ts
+++ packages/eslint-config/src/config.ts
@@ -1,1 +1,2 @@
 export * from './config/jsonc.js';
+export * from './config/imports.js';
--- packages/eslint-config/src/defineConfig.ts
+++ packages/eslint-config/src/defineConfig.ts
@@ -1,22 +1,27 @@
-import { jsonc } from './config.js';
+import { jsonc, imports } from './config.js';
 import { Config } from './type.js';
 
 export interface DefineConfigOptions {
+  import?: boolean | imports.Options;
   jsonc?: boolean | jsonc.Options;
 }
 
 export async function defineConfig(options: DefineConfigOptions = {}) {
+  const importOptions = typeof options.import === 'boolean' ? { enabled: options.import } : { enabled: true, ...options.import };
   const jsoncOptions = typeof options.jsonc === 'boolean' ? { enabled: options.jsonc } : { enabled: true, ...options.jsonc };
 
   let returnValue: Array<Config> = [];
-  const append = async (config: any) =>  {
-    returnValue = [...returnValue, ...await config as any]
+  const append = async (config: any[]) =>  {
+    returnValue = [...returnValue, ...config as any]
   };
 
   if (jsoncOptions.enabled) {
     append(await jsonc(jsoncOptions))
     // sortPackageJson()
     // sortTsconfig()
+  }
+  if (importOptions.enabled) {
+    append(await imports(importOptions))
   }
 
 
