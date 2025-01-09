e03aad83a60ef0cfd5cec528810312b36d76b410
Julien Polo <julien.polo@gmail.com>
2025-01-09 01:13:07 +0100

    📦 Add generated types

--- packages/eslint-config/scripts/typegen.ts
+++ packages/eslint-config/scripts/typegen.ts
@@ -1,5 +1,8 @@
 import fs from 'node:fs/promises';
+import importPlugin from 'eslint-plugin-import';
+import jsdocPlugin from 'eslint-plugin-jsdoc';
 import jsoncPlugin from 'eslint-plugin-jsonc';
+import ymlPlugin from 'eslint-plugin-yml';
 import { pluginsToRulesDTS } from 'eslint-typegen/core';
 
 // eslint-disable-next-line no-console
@@ -9,7 +12,10 @@ await Promise.all(
   (
     [
       // [pluginName, plugin]
+      ['import', importPlugin],
+      ['jsdoc', jsdocPlugin],
       ['jsonc', jsoncPlugin],
+      ['yml', ymlPlugin],
     ] as const
   ).map(async ([pluginName, plugin]) => {
     const content = `/* eslint-disable unicorn/no-abusive-eslint-disable */\n${await pluginsToRulesDTS({
---
+++ packages/eslint-config/src/config/jsonc.ts
@@ -0,0 +1,88 @@
+import { interopDefault, Project } from '@w5s/dev';
+import type { Config } from '../type.js';
+import type {} from 'eslint-plugin-jsonc';
+import type { RuleOptions } from '../typegen/jsonc.js';
+
+const defaultFiles = Project.extensionsToGlob(['.json', '.json5', '.jsonc']);
+const defaultStylistic = { indent: 2 };
+
+export async function jsonc(options: jsonc.Options = {}): Promise<Array<Config>> {
+  const [jsoncPlugin, jsoncParser] = await Promise.all([
+    interopDefault(import('eslint-plugin-jsonc')),
+    interopDefault(import('jsonc-eslint-parser')),
+  ] as const);
+  const { files = defaultFiles, rules = {}, rulesStylistic = true } = options;
+  const { indent } = {
+    ...defaultStylistic,
+    ...(typeof rulesStylistic === 'boolean' ? {} : rulesStylistic),
+  };
+
+  return [
+    {
+      name: 'w5s/jsonc/setup',
+      plugins: {
+        jsonc: jsoncPlugin,
+      },
+    },
+    {
+      files,
+      languageOptions: {
+        parser: jsoncParser,
+      },
+      name: 'w5s/jsonc/rules',
+      rules: {
+        'jsonc/no-bigint-literals': 'error',
+        'jsonc/no-binary-expression': 'error',
+        'jsonc/no-binary-numeric-literals': 'error',
+        'jsonc/no-dupe-keys': 'error',
+        'jsonc/no-escape-sequence-in-identifier': 'error',
+        'jsonc/no-floating-decimal': 'error',
+        'jsonc/no-hexadecimal-numeric-literals': 'error',
+        'jsonc/no-infinity': 'error',
+        'jsonc/no-multi-str': 'error',
+        'jsonc/no-nan': 'error',
+        'jsonc/no-number-props': 'error',
+        'jsonc/no-numeric-separators': 'error',
+        'jsonc/no-octal': 'error',
+        'jsonc/no-octal-escape': 'error',
+        'jsonc/no-octal-numeric-literals': 'error',
+        'jsonc/no-parenthesized': 'error',
+        'jsonc/no-plus-sign': 'error',
+        'jsonc/no-regexp-literals': 'error',
+        'jsonc/no-sparse-arrays': 'error',
+        'jsonc/no-template-literals': 'error',
+        'jsonc/no-undefined-value': 'error',
+        'jsonc/no-unicode-codepoint-escapes': 'error',
+        'jsonc/no-useless-escape': 'error',
+        'jsonc/space-unary-ops': 'error',
+        'jsonc/valid-json-number': 'error',
+        'jsonc/vue-custom-block/no-parsing-error': 'error',
+        ...(rulesStylistic
+          ? {
+              'jsonc/array-bracket-spacing': ['error', 'never'],
+              'jsonc/comma-dangle': ['error', 'never'],
+              'jsonc/comma-style': ['error', 'last'],
+              'jsonc/indent': ['error', indent],
+              'jsonc/key-spacing': ['error', { afterColon: true, beforeColon: false }],
+              'jsonc/object-curly-newline': ['error', { consistent: true, multiline: true }],
+              'jsonc/object-curly-spacing': ['error', 'always'],
+              'jsonc/object-property-newline': ['error', { allowMultiplePropertiesPerLine: true }],
+              'jsonc/quote-props': 'error',
+              'jsonc/quotes': 'error',
+            }
+          : {}),
+        ...rules,
+      },
+    },
+  ];
+}
+
+export namespace jsonc {
+  export type Rules = RuleOptions;
+
+  export interface Options {
+    files?: Array<string>;
+    rules?: Rules;
+    rulesStylistic?: boolean;
+  }
+}
