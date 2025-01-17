3e6278ceac5546120a58574e69543d349ee41b7b
Julien Polo <julien.polo@gmail.com>
2025-01-17 10:24:28 +0100

    ✅ Fix tests

--- packages/dev/src/Project.spec.ts
+++ packages/dev/src/Project.spec.ts
@@ -1,5 +1,5 @@
 import { describe, expect, it } from 'vitest';
-import { Project } from './project.js';
+import { Project } from './Project.js';
 
 describe('Project', () => {
   describe('.ecmaVersion()', () => {
