06c9b502f5e4de256e78109a7baef94e2b3b3af8
Julien Polo <julien.polo@gmail.com>
2025-04-14 15:57:23 +0200

    ♻️ Remove excluded imports
    Reason : no visible positive impact on performances

--- packages/cspell-config/src/index.ts
+++ packages/cspell-config/src/index.ts
@@ -4,23 +4,8 @@ import cSpellSettings from '@cspell/cspell-bundled-dicts/cspell-default.config.j
 
 const toArray = <T>(value: T | T[] | undefined) => (Array.isArray(value) ? value : value == null ? [] : [value]);
 const defaultSettings = cSpellSettings as AdvancedCSpellSettings;
-const excludeImport = new Set([
-  '@cspell/dict-ada/cspell-ext.json',
-  '@cspell/dict-cpp/cspell-ext.json',
-  '@cspell/dict-csharp/cspell-ext.json',
-  '@cspell/dict-django/cspell-ext.json',
-  '@cspell/dict-dotnet/cspell-ext.json',
-  '@cspell/dict-elixir/cspell-ext.json',
-  '@cspell/dict-fsharp/cspell-ext.json',
-  '@cspell/dict-haskell/cspell-ext.json',
-  '@cspell/dict-latex/cspell-ext.json',
-  '@cspell/dict-lua/cspell-ext.json',
-  '@cspell/dict-monkeyc/cspell-ext.json',
-  '@cspell/dict-php/cspell-ext.json',
-  '@cspell/dict-powershell/cspell-ext.json',
-  '@cspell/dict-python/cspell-ext.json',
-  '@cspell/dict-r/cspell-ext.json',
-  '@cspell/dict-scala/cspell-ext.json',
+const excludeImport = new Set<string>([
+  // Put here the imports you want to exclude
 ]);
 const dictionaryDefinitions: AdvancedCSpellSettings['dictionaryDefinitions'] = [
   {
