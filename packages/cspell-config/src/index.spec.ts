5e5ab679f2750688c48abcfb8eeeb44d8a81e067
Julien Polo <julien.polo@gmail.com>
2025-05-06 10:33:02 +0200

    💚 Fix build deps

--- packages/cspell-config/src/index.spec.ts
+++ packages/cspell-config/src/index.spec.ts
@@ -16,7 +16,6 @@ describe('index', () => {
         '@cspell/dict-docker/cspell-ext.json',
         '@cspell/dict-en_us/cspell-ext.json',
         '@cspell/dict-en-common-misspellings/cspell-ext.json',
-        '@cspell/dict-en-gb/cspell-ext.json',
         '@cspell/dict-filetypes/cspell-ext.json',
         '@cspell/dict-flutter/cspell-ext.json',
         '@cspell/dict-fonts/cspell-ext.json',
