f7373cbfc5c2f13b4a0cce20b24fc4f69727ef92
Julien Polo <julien.polo@gmail.com>
2026-07-01 11:30:52 +0200

    🔧 Simplify ESLint configuration export

--- eslint.config.js
+++ eslint.config.js
@@ -1,25 +1,16 @@
+// @ts-check
 import { defineConfig } from '@w5s/eslint-config';
 
-export default [
-  ...(await defineConfig({
-    ignores: [
-      '**/dist',
-      '**/CHANGELOG.md',
-      '**/CODE_OF_CONDUCT.md',
-      '**/.temp/**',
-    ],
-  })),
-  {
-    // parserOptions: {
-    //   project: './tsconfig.json',
-    // },
-    rules: {
-      'node/no-sync': 'off',
-      'import/no-unresolved': 'off',
-      'unicorn/prefer-module': 'off',
-    },
-    // plugins: [
-    //   'sort-keys'
-    // ],
+export default await defineConfig({
+  ignores: [
+    '**/dist',
+    '**/CHANGELOG.md',
+    '**/CODE_OF_CONDUCT.md',
+    '**/.temp/**',
+  ],
+  rules: {
+    'node/no-sync': 'off',
+    'import/no-unresolved': 'off',
+    'unicorn/prefer-module': 'off',
   },
-];
+});
