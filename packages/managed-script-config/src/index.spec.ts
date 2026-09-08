42b18b8501313fd13636a81e73ee94c6f9df6437
Julien Polo <julien.polo@gmail.com>
2026-09-08 16:56:10 +0200

    ✅ Refactor tests

--- packages/managed-script-config/src/index.spec.ts
+++ packages/managed-script-config/src/index.spec.ts
@@ -4,12 +4,19 @@ import * as Module from './index.js';
 
 describe('index', () => {
   it('should match snapshot', () => {
-    const { meta, ...rest } = Module;
-    expect(rest).toMatchSnapshot();
+    const { meta } = Module;
     expect(meta).toEqual({
       buildNumber: expect.any(Number),
       name: expect.any(String),
       version: expect.any(String),
+    });
+    expect(new Set(Object.keys(Module))).toEqual(new Set(['default', 'meta']));
+  });
+  it('should export configuration scripts', () => {
+    expect(Module.default).toMatchObject({
+      scripts: {
+        rescue: expect.any(String),
+      },
     });
   });
 });
