f5f3526c530301b618146fc656672635b9dfdf5c
Julien Polo <julien.polo@gmail.com>
2026-09-08 16:26:17 +0200

    ✨ Add defineConfig helper

--- packages/managed-script/src/index.spec.ts
+++ packages/managed-script/src/index.spec.ts
@@ -4,6 +4,12 @@ import * as Module from './index.js';
 
 describe('index', () => {
   it('exports', () => {
-    expect(new Set(Object.keys(Module))).toEqual(new Set(['ManagedScript', 'ManagedScriptCommand', 'ManagedScriptEnv', 'meta']));
+    expect(new Set(Object.keys(Module))).toEqual(new Set([
+      'defineConfig',
+      'ManagedScript',
+      'ManagedScriptCommand',
+      'ManagedScriptEnv',
+      'meta',
+    ]));
   });
 });
