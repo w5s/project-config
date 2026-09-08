f193fd1a7a08d8cbb1e0357595758d9a392d1a8f
Julien Polo <julien.polo@gmail.com>
2026-09-08 16:26:17 +0200

    ✨ Add defineConfig helper

--- packages/managed-script/src/ManagedScript/resolveScripts.ts
+++ packages/managed-script/src/ManagedScript/resolveScripts.ts
@@ -1,7 +1,7 @@
 import path from 'node:path/win32';
 
-import type { ResolvedConfig } from '../config/ResolvedConfig.js';
 import type { ConfigLoaderResult } from '../infrastructure/ConfigLoader.js';
+import type { ResolvedConfig } from '../type/ResolvedConfig.js';
 
 export function resolveScripts(loaded: ConfigLoaderResult): ResolvedConfig {
   const scripts: ResolvedConfig['scripts'] = {};
--- packages/managed-script/src/index.ts
+++ packages/managed-script/src/index.ts
@@ -1,3 +1,4 @@
+export * from './defineConfig.js';
 export { ManagedScript } from './ManagedScript.js';
 export { ManagedScriptEnv } from './ManagedScriptEnv.js';
 export * from './meta.js';
